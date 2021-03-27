d417bfa75c93acaf03034d3bf616b3c762a07027
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-03-27 11:54:23 -0600

    trying to get race

--- public/js/graphRace.js
+++ public/js/graphRace.js
@@ -1,45 +1,66 @@
-var trace1 = {
-    x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
-    // Y axis needs to be a variable 
-    y: [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17, 10],
-    type: 'bar',
-    name: 'Pfizer',
-    marker: {
-        color: 'rgb(49,130,189)',
+const raceInput;
+
+$(document).ready(() => {
+
+    $("#race-btn").on("click", event => {
+        event.preventDefault();
+        raceInput = $("#raceDrop").val();
+        raceValues(raceInput);
+        raceInput.val("");
+    });
+
+    function raceValues(race) {
+        fetch(`/api/race-graph/${race}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', },
+        }).then(function (response) {
+            console.log(response.json());
+            drawGraph(response.json());
+        })
     }
-};
 
-var trace2 = {
-    x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
-    // Y axis needs to be a variable
-    y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16, 5],
-    type: 'bar',
-    name: 'Moderna',
-    marker: {
-        color: 'rgb(214,43,29)',
+
+    function drawGraph(data){
+        var trace1 = {
+            x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
+            // Y axis needs to be a variable 
+            y: data.pfizerRace,
+            type: 'bar',
+            name: 'Pfizer',
+            marker: {
+                color: 'rgb(49,130,189)',
+            }
+        };   
+        var trace2 = {
+            x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
+            // Y axis needs to be a variable
+            y: data.modernaRace,
+            type: 'bar',
+            name: 'Moderna',
+            marker: {
+                color: 'rgb(214,43,29)',
+            }
+        };
+        var trace3 = {
+            x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
+            // Y axis needs to be a variable.
+            y: data.jandjRace,
+            type: 'bar',
+            name: 'Johnson & Johnson',
+            marker: {
+                color: 'rgb(42,234,110)',
+            }
+        };
+        var data = [trace1, trace2, trace3];
+
+        var layout = {
+            title: 'Age Symptoms',
+            xaxis: {
+                tickangle: -45
+            },
+            barmode: 'group'
+        };
+    
+        Plotly.newPlot('myDiv', data, layout);
     }
-};
-
-var trace3 = {
-    x: ['Pain at injection site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Enlarged Glands', 'Nausea & Vomiting', 'Vomiting', 'Chills', 'Swelling', 'Skin Redness', 'Fever', 'No Symptoms'],
-    // Y axis needs to be a variable.
-    y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16, 10],
-    type: 'bar',
-    name: 'Johnson & Johnson',
-    marker: {
-        color: 'rgb(42,234,110)',
-    }
-};
-
-var data = [trace1, trace2, trace3];
-
-var layout = {
-    title: 'Age Symptoms',
-    xaxis: {
-        tickangle: -45
-    },
-    barmode: 'group'
-};
-
-
-Plotly.newPlot('myDiv', data, layout);
+});
