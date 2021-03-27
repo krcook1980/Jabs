33530da9559eb6165aa77a770234f50849c86777
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-03-26 18:32:38 -0600

    New stuff coming in hot

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,67 @@
+
+
 $(document).ready(() => {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(data => {
-      $(".admin-username").text(data.username);
-    });
+  const manageForm = $("form.manage");
+  const IDInput = $("input#id-input");
+
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(data => {
+    $(".admin-username").text(data.username);
   });
-  
+
+  $("#delete-btn").on("click", event => {
+    event.preventDefault();
+    const idData = {
+      id: IDInput.val().trim(),
+    };
+
+    
+    if (!idData.id) {
+      console.log("return");
+      return;
+    }
+    // If we have an ID, we run the deleteEntry function and clear the form
+    deleteEntry(idData.id);
+    IDInput.val("");
+  });
+
+  function deleteEntry(id) {
+    
+    fetch(`/api/users/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json', },
+    }).then(function(){
+      alert("Entry has been deleted")
+    });
+  }
+
+  $("#display-btn").on("click", event => {
+    event.preventDefault();
+    const idData = {
+      id: IDInput.val().trim(),
+    };
+
+    
+    if (!idData.id) {
+      console.log("return");
+      return;
+    }
+    // If we have an ID, we run the deleteEntry function and clear the form
+    displayEntry(idData.id);
+    IDInput.val("");
+  });
+
+  function displayEntry(id){
+    fetch(`/api/users/${id}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', },
+    }).then(function(userData){
+     
+     return userData.json();
+    })
+    .then(function(userData){
+      console.log(userData);
+    })
+  }
+  });
