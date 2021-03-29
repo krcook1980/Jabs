7d67bcf558f6417b24041c583a541cf704d8b089
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-03-29 13:12:53 -0600

    admin seeder file

--- seeders/20210323014218-administators.js
+++ seeders/20210323014218-administators.js
@@ -2,23 +2,51 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Administrators', [
+      {
+        username: 'KellyH',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'KellyC',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Andrew',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Ricky',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Eloy',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Spencer',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+    await queryInterface.bulkDelete('Administrators', null, {});
+
   }
 };
+
