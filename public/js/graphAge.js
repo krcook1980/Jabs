fd515aaa53508a4424fae272ac9d637cff7fbcc5
Andrew Burke <Drewburke39@gmail.com>
2021-03-29 21:48:03 -0600

    changed syntax for age range on statistics,handlebars and updated the fetch and on click function

--- public/js/graphAge.js
+++ public/js/graphAge.js
@@ -1,64 +1,93 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    $("#age-btn").on("click", event => {
+    $("#ageSubmit").on("click", event => {
         event.preventDefault();
-        ageInput = $("#ageDrop").val();
-        ageInput.val("");
+        ageInput = $("#inputAge").val();
+        console.log("I am Andrew questions " + ageInput)
+        ageValues(ageInput);
+        // ageInput.val("");
     });
 
-    var trace1 = {
-        x:['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
-        // Y axis needs to be a variable 
-        y: [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17, 10],
-        type: 'bar',
-        name: 'Pfizer',
-        marker: {
-            color: 'rgb(49,130,189)',
-        }
-    };
+    function ageValues(age) {
+        fetch(`/api/age-graph/${age}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', },
+        }).then(function (ageInput) {
 
-    var trace2 = {
-        x:['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
-        // Y axis needs to be a variable
-        y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16, 5],
-        type: 'bar',
-        name: 'Moderna',
-        marker: {
-            color: 'rgb(214,43,29)',
-        }
-    };
+            return ageInput.json();
+        })
+            .then(function (ageInput) {
+                console.log("I am in raceValues userData " + ageInput)
+                let yModerna = Object.values(ageInput[0])
+                yModerna.pop()
+                yModerna = yModerna.map(Number)
+                console.log(yModerna)
+                let yPfizer = Object.values(ageInput[1])
+                yPfizer.pop()
+                yPfizer = yPfizer.map(Number)
+                console.log(yPfizer)
+                let yjandj = Object.values(ageInput[2])
+                yjandj.pop()
+                yjandj = yjandj.map(Number)
+                console.log(yjandj)
+                drawGraph(yModerna, yPfizer, yjandj)
+            })
+    }
 
-    var trace3 = {
-        x:['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
-        // Y axis needs to be a variable.
-        y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16, 10],
-        type: 'bar',
-        name: 'Johnson & Johnson',
-        marker: {
-            color: 'rgb(42,234,110)',
-        }
-    };
+    function drawGraph(yModerna, yPfizer, yjandj) {
+        console.log("I'm inside draw " + yModerna)
+        var trace1 = {
+            x: ['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
+            // Y axis needs to be a variable 
+            y: yPfizer,
+            type: 'bar',
+            name: 'Pfizer',
+            marker: {
+                color: 'rgb(49,130,189)',
+            }
+        };
 
-    var data = [trace1, trace2, trace3];
+        var trace2 = {
+            x: ['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
+            // Y axis needs to be a variable
+            y: yModerna,
+            type: 'bar',
+            name: 'Moderna',
+            marker: {
+                color: 'rgb(214,43,29)',
+            }
+        };
 
-    var layout2 = {
-        title: 'Common Symptoms By Age Group',
-        xaxis: {
-            tickangle: -25,
-            color: 'rgb(3,49, 140)',
-        },
-        yaxis: {
-            range: [1,40],
-            color: 'rgb(3,49, 140)',
-        },
+        var trace3 = {
+            x: ['Pain at Site', 'Fatigue', 'Headache', 'Muscle Soreness', 'Joint Pain', 'Nausea', 'Vomiting', 'Chills', 'Swelling', 'Rash', 'Fever', 'Severe Allergic Reaction', 'No Symptoms'],
+            // Y axis needs to be a variable.
+            y: yjandj,
+            type: 'bar',
+            name: 'Johnson & Johnson',
+            marker: {
+                color: 'rgb(42,234,110)',
+            }
+        };
 
-        barmode: 'group'
-    };
+        var data = [trace1, trace2, trace3];
 
-    Plotly.newPlot('myDiv2', data, layout2);
+        var layout2 = {
+            title: 'Common Symptoms By Age Group',
+            xaxis: {
+                tickangle: -25,
+                color: 'rgb(3,49, 140)',
+            },
+            yaxis: {
+                range: [1, 40],
+                color: 'rgb(3,49, 140)',
+            },
 
+            barmode: 'group'
+        };
+
+        Plotly.newPlot('myDiv2', data, layout2);
+    }
 
 });
