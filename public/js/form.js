597498280a3fc63b921dba97e2c9302dfd1d88f9
Ricky Panh <ricky@dragonflyut.com>
2021-03-29 20:49:29 -0600

    final fix for form, fixed alert and redirect

--- public/js/form.js
+++ public/js/form.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (submitSurvey) {
         submitSurvey.addEventListener('submit', (e) => {
             e.preventDefault();
+
             for (let checkbox of document.querySelectorAll('input[type=checkbox]')) {
                 checkbox.value = checkbox.checked ? 1 : 0;
                 checkbox.addEventListener('change', e => {
@@ -23,10 +24,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 age: age
             };
             // console.log(userData)
-            
-            let pain1 = document.getElementById('symptomInjectionPain1').value
+
+            let pain1 = document.getElementById('symptomInjectionPain1').value.trim(),
                 painOne = parseInt(pain1)
-                console.log("I am parse pain " + painOne)
+            console.log("I am parse pain " + painOne)
             let fever1 = document.getElementById('symptomFever1').value.trim(),
                 feverOne = parseInt(fever1)
             let fatigue1 = document.getElementById('symptomFatigue1').value.trim(),
@@ -49,40 +50,40 @@ document.addEventListener('DOMContentLoaded', () => {
                 vomitingOne = parseInt(vomiting1)
             let no_symptoms1 = document.getElementById('noSymptoms1').value.trim(),
                 no_symptomsOne = parseInt(no_symptoms1)
-            let pain2 = document.getElementById('symptomInjectionPain1').value
-                painTwo = parseInt(pain1)
-            let fever2 = document.getElementById('symptomFever1').value.trim(),
+            let pain2 = document.getElementById('symptomInjectionPain2').value.trim(),
+                painTwo = parseInt(pain2)
+            let fever2 = document.getElementById('symptomFever2').value.trim(),
                 feverTwo = parseInt(fever2)
-            let fatigue2 = document.getElementById('symptomFatigue1').value.trim(),
+            let fatigue2 = document.getElementById('symptomFatigue2').value.trim(),
                 fatigueTwo = parseInt(fatigue2)
-            let rash2 = document.getElementById('symptomRash1').value.trim(),
+            let rash2 = document.getElementById('symptomRash2').value.trim(),
                 rashTwo = parseInt(rash2)
-            let joint_pain2 = document.getElementById('symptomJointPain1').value.trim(),
+            let joint_pain2 = document.getElementById('symptomJointPain2').value.trim(),
                 joint_painTwo = parseInt(joint_pain2)
-            let headache2 = document.getElementById('symptomHeadache1').value.trim(),
+            let headache2 = document.getElementById('symptomHeadache2').value.trim(),
                 headacheTwo = parseInt(headache2)
-            let muscle_soreness2 = document.getElementById('symptomMuscleAches1').value.trim(),
+            let muscle_soreness2 = document.getElementById('symptomMuscleAches2').value.trim(),
                 muscle_sorenessTwo = parseInt(muscle_soreness2)
-            let chills2 = document.getElementById('symptomChills1').value.trim(),
+            let chills2 = document.getElementById('symptomChills2').value.trim(),
                 chillsTwo = parseInt(chills2)
-            let nausea2 = document.getElementById('symptomNausea1').value.trim(),
+            let nausea2 = document.getElementById('symptomNausea2').value.trim(),
                 nauseaTwo = parseInt(nausea2)
-            let swelling2 = document.getElementById('symptomSwelling1').value.trim(),
+            let swelling2 = document.getElementById('symptomSwelling2').value.trim(),
                 swellingTwo = parseInt(swelling2)
-            let vomiting2 = document.getElementById('symptomVomiting1').value.trim(),
+            let vomiting2 = document.getElementById('symptomVomiting2').value.trim(),
                 vomitingTwo = parseInt(vomiting2)
-            let no_symptoms2 = document.getElementById('noSymptoms1').value.trim(),
+            let no_symptoms2 = document.getElementById('noSymptoms2').value.trim(),
                 no_symptomsTwo = parseInt(no_symptoms2)
             let severe_reaction1 = document.getElementById('symptomAllergicReaction1').value.trim(),
-                severe_reactionOne = parseInt(severe_reaction1)   
-            let severe_reaction2 = document.getElementById('symptomAllergicReaction1').value.trim(),
-                severe_reactionTwo = parseInt(severe_reaction2) 
+                severe_reactionOne = parseInt(severe_reaction1)
+            let severe_reaction2 = document.getElementById('symptomAllergicReaction2').value.trim(),
+                severe_reactionTwo = parseInt(severe_reaction2)
 
             const symptomData1 = {
-                pain_at_site1: painOne, 
+                pain_at_site1: painOne,
                 fatigue1: fatigueOne,
                 headache1: headacheOne,
-                muscle_soreness1: muscle_sorenessOne,  
+                muscle_soreness1: muscle_sorenessOne,
                 joint_pain1: joint_painOne,
                 nausea1: nauseaOne,
                 vomiting1: vomitingOne,
@@ -95,10 +96,10 @@ document.addEventListener('DOMContentLoaded', () => {
             };
             // console.log(symptomData1)
             const symptomData2 = {
-                pain_at_site2: painTwo, 
+                pain_at_site2: painTwo,
                 fatigue2: fatigueTwo,
                 headache2: headacheTwo,
-                muscle_soreness2: muscle_sorenessTwo,  
+                muscle_soreness2: muscle_sorenessTwo,
                 joint_pain2: joint_painTwo,
                 nausea2: nauseaTwo,
                 vomiting2: vomitingTwo,
@@ -136,13 +137,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 // make sure to serialize the JSON body
                 body: JSON.stringify(newSurvey),
-
-            }).then((res) => {
-                alert("Thank you, your data was received")
-
-                window.location.replace('/statistics');
-
             })
+            alert("Thank you, your data was received")
+            window.location.replace('/statistics');
         })
+    } else {
+        return console.log("Your data has been recorded")
     }
 })
